171a029778a5f7b0914c5c938ef237aaa3a9b8e5
seryafarma <enhope.estheim@gmail.com>
2023-04-14 18:30:02 +0200

    Copy the weather class

--- weather_clock.py
+++ weather_clock.py
@@ -12,6 +12,45 @@ from luma.core.render import canvas
 from luma.core.legacy import text, show_message
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT
 
+import json
+import urllib.request
+
+# =============================================================================
+class Weather:
+    ''' Get the API and build the string.
+    '''
+    def __init__(self) -> None:
+        self.weather = self.weather_description = self.feels_like = self.temperature = self.humidity = self.name = ""
+        self.addr = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + api_key
+        self.y = None
+
+    def get_weather(self) -> None:
+        with urllib.request.urlopen(self.addr) as url:
+            self.y = json.load(url)
+        
+    def interpret_weather(self) -> None:
+        self.weather = self.y['weather'][0]['main']
+        self.weather_description = self.y['weather'][0]['description']
+        self.feels_like = f"feels like: {self.y['main']['feels_like'] - 273.15:.2f} C"
+        self.temperature = f"temperature: {self.y['main']['temp'] - 273.15:.2f} C"
+        self.humidity = f"humidity: {self.y['main']['humidity']} %"
+        self.name = f"{self.y['name']}"
+
+# =============================================================================
+class Stringifier:
+    ''' Convert the weather to a string
+    '''
+    def __init__(self) -> None:
+        self.weather = Weather()
+
+    def build_string(self) -> str:
+        self.weather.get_weather()
+        self.weather.interpret_weather()
+        return f"{self.weather.name} = {self.weather.weather}: {self.weather.weather_description}, {self.weather.feels_like}, {self.weather.temperature}, {self.weather.humidity}"
+
+a = Stringifier()
+print(a.build_string())
+
 # =============================================================================
 class Displayer:
     ''' Display the hours minutes seconds on the MAX7219.
